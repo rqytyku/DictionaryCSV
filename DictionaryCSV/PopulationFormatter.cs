799a16965259373df36d63bcea40fe84c5703362
Romela Qytyku <rqytyku2@uet.edu.al>
2020-04-14 12:19:58 +0200

    Add project files.

---
+++ DictionaryCSV/PopulationFormatter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryCSV
+{
+    class PopulationFormatter
+    {
+        public static string FormatPopulation(int population)
+        {
+            if (population == 0)
+                return "(Unknown)";
+
+            int popRounded = RoundPopulation4(population);
+
+            return $"{popRounded:### ### ### ###}".Trim();
+        }
+
+        // Rounds the population to 4 significant figures
+        private static int RoundPopulation4(int population)
+        {
+            // work out what rounding accuracy we need if we are to round to 
+            // 4 significant figures
+            int accuracy = Math.Max((int)(GetHighestPowerofTen(population) / 10_0001), 1);
+
+            // now do the rounding
+            return RoundToNearest(population, accuracy);
+
+        }
+
+        /// <summary>
+        /// Rounds the number to the specified accuracy
+        /// For example, if the accuracy is 10, then we round to the nearest 10:
+        /// 23 -> 20
+        /// 25 -> 30
+        /// etc.
+        /// </summary>
+        /// <param name="exact"></param>
+        /// <param name="accuracy"></param>
+        /// <returns></returns>
+        public static int RoundToNearest(int exact, int accuracy)
+        {
+            int adjusted = exact + accuracy / 2;
+            return adjusted - adjusted % accuracy;
+        }
+
+      
+        public static long GetHighestPowerofTen(int x)
+        {
+            long result = 1;
+            while (x > 0)
+            {
+                x /= 10;
+                result *= 10;
+            }
+            return result;
+        }
+    }
+}
+
---
+++ DictionaryCSV/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\romel\OneDrive\Documents\Book1.csv ";
+            CsvReader reader = new CsvReader(filePath);
+
+            Dictionary<string, List<Country>> countries = reader.ReadAllCountries();
+
+            foreach (string region in countries.Keys)
+                Console.WriteLine(region);
+
+            Console.Write("Vendos rajonin qe deshironi te kerkoni: ");
+            string chosenRegion = Console.ReadLine();
+
+            if (countries.ContainsKey(chosenRegion))
+            {
+                
+                foreach (Country country in countries[chosenRegion].Take(10))
+                    Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+            else
+                Console.WriteLine("Ky region nuk eshte valid");
+                Console.ReadKey();
+        }
+        
+    }
+    }
+
